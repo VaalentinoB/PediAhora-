e0ca62d33235fe1324d0b8a3fbed1f75fc74610b
VaalentinoB <valentinoburioni@hotmail.com>
2024-05-06 21:42:44 -0300

    iniciando a darle forma a las cards

--- src/Item.jsx
+++ src/Item.jsx
@@ -2,11 +2,17 @@ import "./ItemList";
 
 const Item = ({ item }) => {
   return (
-    <div className="item-card">
-      <img src={item.imageUrl} alt="item.name" className="item-image" />
-      <div className="item-details">
-        <h3 className="item-name">{item.name}</h3>
-        <p className="item-price">Precio: ${item.price}</p>
+    <div class="container">
+      <div class="wrapper">
+        <img src={item.imageUrl} alt="item.name" className="banner-image" />
+        <div className="item-details">
+          <h3 className="item-name">{item.name}</h3>
+          <p className="item-price">Precio: ${item.price}</p>
+          <div class="button-wrapper">
+            <button class="btn outline">DETAILS</button>
+            <button class="btn fill">BUY NOW</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route exact path="*" element={<NotFound />} />
-        <Route exact path="/soporte" element={<support />} />
+        <Route exact path="/support" element={<support />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
